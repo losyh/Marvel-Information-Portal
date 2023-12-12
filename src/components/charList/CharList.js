6e940b9be54f950e4f23cbce0717463d96a1aa7f
Aleksey Petrov <losyh2005@mail.ru>
2023-12-12 20:05:54 +0400

    Add char list

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,12 +1,17 @@
 import { Component } from 'react/cjs/react.production.min';
 import './charList.scss';
 import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/spinner';
+import ErrorMesage from '../errorMessage/errorMesage';
 
 class CharList extends Component {
 
     state = {
-        char: {}
+        char: {},
+        loading: true,
+        error: false
     }
+
     marvelService = new MarvelService();
 
     componentDidMount() {
@@ -16,32 +21,35 @@ class CharList extends Component {
     componentWillUnmount(){
 
     }
+
     onCharLoaded = (char) => {
-        this.setState({char})
+        this.setState({char, loading: false})
+    }
+
+    OnError = () => {
+        this.setState({loading: false, error: true})
     }
 
     updateChar = () => {
+        this.setState({loading: true})
         this.marvelService
         .getAllCharacters()
         .then(this.onCharLoaded)
+        .catch(this.OnError)
     }
 
 
         render() {
-        const allChars = this.state.char
-        const chars = allChars.map(item => {
-            return (
-                <li className="char__item">
-                        <img src={item.thumbnail} alt={item.name}/>
-                        <div className="char__name">{item.name}</div>
-                    </li>
-            )
-        }) 
-        console.log(this.state.char)
+            const {char, loading, error} = this.state
+            const errorMesage = error ? <ErrorMesage/> : null
+            const spinner = loading ? <Spinner/> : null
+            const content = !(loading || error) ? <View char={char}/> : null
             return (
                 <div className="char__list">
                     <ul className="char__grid">
-                     {chars}
+                    {errorMesage}
+                    {spinner}
+                    {content}
                     </ul>
                     <button className="button button__main button__long">
                         <div className="inner">load more</div>
@@ -49,6 +57,20 @@ class CharList extends Component {
                 </div>
             )
         }
+    }
+
+
+    const View = ({char}) => {
+
+        const charList = char.map((item, index) => {
+            return (
+                <li className="char__item" key={index}>
+                <img src={item.thumbnail} alt={item.name}/>
+                <div className="char__name">{item.name}</div>
+            </li>
+            )
+        })
+        return charList
     }
 
 
